8a6a6b1ca29510cae1943cb9472d017abe049cb5
GuiZhouAndroid <2393579405@qqcom>
2021-09-29 22:28:49 +0800

    AndroidX viewPager+Fragment 懒加载

---
+++ app/src/main/java/cn/bluemobi/dylan/viewpagerfragmentlazyload/MainActivity.java
@@ -0,0 +1,59 @@
+package cn.bluemobi.dylan.viewpagerfragmentlazyload;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RadioGroup rg;
+    private ViewPager vp;
+
+    private void assignViews() {
+        rg = (RadioGroup) findViewById(R.id.rg);
+        vp = (ViewPager) findViewById(R.id.vp);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        assignViews();
+        initData();
+    }
+
+    private void initData() {
+        final List<Fragment> fragments = new ArrayList<>();
+        fragments.add(new Fragment1());
+        fragments.add(new Fragment2());
+        fragments.add(new Fragment3());
+        fragments.add(new Fragment4());
+        vp.setOffscreenPageLimit(0);
+        vp.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return fragments.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+        });
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                vp.setCurrentItem(radioGroup.indexOfChild(radioGroup.findViewById(i)));
+            }
+        });
+    }
+
+}
